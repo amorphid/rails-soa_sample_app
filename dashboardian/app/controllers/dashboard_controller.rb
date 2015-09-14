3a5889b14ccb259b19e0c5bab22c1e55bc7c9d07
Michael Pope <michael.pope@livingsocial.com>
2015-09-14 15:16:12 -0400

    Add http call to dashboardian to get rants

--- dashboardian/app/controllers/dashboard_controller.rb
+++ dashboardian/app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    uri = URI('http://localhost:3001/rants')
+    request = Net::HTTP::Get.new(uri)
+    request.content_type = 'application/json'
+    response = Net::HTTP.start(uri.hostname, uri.port) {|http| http.request(request) }
   end
 end
